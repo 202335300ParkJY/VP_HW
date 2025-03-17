97a56c842be3f5547306880a6eb57887f335479d
202335300ParkJY <163634389+202335300ParkJY@users.noreply.github.com>
2025-03-17 11:49:04 +0900

    Add files via upload

---
+++ week3-1/Custom_dialog.py
@@ -0,0 +1,38 @@
+import sys
+from PySide6.QtWidgets import (
+    QApplication,
+    QDialog,
+    QDialogButtonBox,
+    QVBoxLayout, QLabel,
+    QMainWindow,
+    QPushButton,
+)
+
+class CustomDlg(QDialog): 
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle('Hello, QDialog')
+
+        buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel #파이프, 바티컬 바 존재
+        # 이를 통해 한 명령어의 출력을 다른 명령어의 입력으로 전달.
+        # buttons에 해당하는 button 객체
+        self.button_box = QDialogButtonBox(buttons) 
+
+        # QDialog의 메서드를 slot으로
+        self.button_box.accepted.connect(self.accept) 
+        # QDialog의 메서드를 slot으로
+        self.button_box.rejected.connect(self.reject)    
+
+        self.layout = QVBoxLayout()
+        message = QLabel('Is something ok?')
+        self.layout.addWidget(message)
+        # QDialogButtonBox 객체 추가.
+        self.layout.addWidget(self.button_box) 
+        self.setLayout(self.layout)
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = CustomDlg()
+    window.show()
+    app.exec()
---
+++ week3-1/modal_Qdialog_test.py
@@ -0,0 +1,39 @@
+import sys
+from PySide6.QtWidgets import (
+    QApplication,
+    QDialog,
+    QDialogButtonBox,
+    QVBoxLayout, QLabel,
+    QMainWindow,
+    QPushButton,
+)
+
+class MW(QMainWindow): 
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QDialog Ex.")
+
+        button = QPushButton("Press it for a Dialog")
+        button.clicked.connect(self.button_clicked)
+
+        self.setCentralWidget(button) #이 다음에 show가 없으므로 마지막에 꼭 넣어야 함.
+
+    def button_clicked(self, s): #MW의 인스턴스 항목임. self가 들어갔기 때문.
+        print("click", s)
+        dlg = QDialog(self) #self,부모
+        dlg.setWindowTitle("QDialog Title") 
+        dlg.exec() #blocking mode로 동작
+
+        # -------------
+        # for custom dlg
+        # dlg = CustomDlg(self) 
+        # if dlg.exec(): # Modal Dialog
+        #     print('ok')
+        # else:
+        #     print("cancel")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MW()
+    window.show() #중요
+    app.exec()
