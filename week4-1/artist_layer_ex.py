dd1e6aa58222ff189207b3e684cc47d89cef2b57
202335300ParkJY <163634389+202335300ParkJY@users.noreply.github.com>
2025-03-26 10:37:17 +0900

    Add files via upload

---
+++ week4-1/artist_layer_ex.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+# 데이터 준비
+x = [1,2,3,4,5]
+y = [1,2,3,4,5]
+
+# Figure & Axes 객체 생성
+fig, ax = plt.subplots()
+
+# 그래프 생성
+line, = ax.plot(x, y, 'r-') # 'r-'는 red solid line 의미
+
+# 그래프 제목 추가
+ax.set_title('simple line plot')
+
+# X,y 축 레이블 추가
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+
+# 그래프 출력
+plt.show()
